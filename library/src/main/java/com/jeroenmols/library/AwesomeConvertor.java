722dd32b5b183ae778195a31b07ec7e240292953
Jeroen Mols <jeroen.mols.be@gmail.com>
2015-12-15 14:59:03 +0100

    Simplified library

--- library/src/main/java/com/jeroenmols/library/AwesomeConvertor.java
+++ library/src/main/java/com/jeroenmols/library/AwesomeConvertor.java
@@ -1,14 +1,11 @@
 package com.jeroenmols.library;
 
-import com.google.common.base.CharMatcher;
-
 /**
  * Created by Jeroen Mols on 06/08/15.
  */
 public class AwesomeConvertor {
 
     public static String toAwesome(String nonAwesomeString) {
-        int count = CharMatcher.DIGIT.countIn(nonAwesomeString);
-        return String.format("%dx Awesome!", count);
+        return nonAwesomeString.replaceAll("\\w+", "Awesome!");
     }
 }
